70723314a224ff4cc238243ac1a3a8a75833d820
Jaehyun Park <hirouth@naver.com>
2026-02-24 12:24:12 +0900

    feat: pretendard 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,38 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import Header from '@/shared/components/layout/header';
 import Footer from '@/shared/components/layout/footer';
+import { ReactNode } from 'react';
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  variable: '--font-pretendard',
+  display: 'swap',
+  weight: '100 900',
+  fallback: [
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'sans-serif',
+  ],
+});
 
 export const metadata: Metadata = {
-  title: 'Dev Match',
-  description: 'Dev Match',
+  title: 'MONOLINK',
+  description: 'MONOLINK',
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body className="flex min-h-screen flex-col">
-        <Header user={{ nickname: 'John Doe', avatarUrl: null }} />
+        <Header />
         <main className="flex-1 py-4">{children}</main>
         <Footer />
       </body>
